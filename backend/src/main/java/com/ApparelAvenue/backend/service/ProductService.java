b96c7f85853046836e51e903eaec7bec6a8247ac
gudulu <rrnayak165@gmail.com>
2024-09-18 07:50:55 +0530

    product service add

---
+++ backend/src/main/java/com/ApparelAvenue/backend/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.ApparelAvenue.backend.service;
+
+import java.util.List;
+
+import com.ApparelAvenue.backend.model.Product;
+
+public interface ProductService {
+    Product createProduct();
+
+    Product updateProduct(String id, Product newProduct);
+
+    void deleteAllProduct();
+
+    Product deleteProductById(String id);
+
+    Product increaseProductQuantity(String id, int quantity);
+
+    Product decreaseProductQuantity(String id, int quantity);
+
+    Product updateProductPrice(String id, double price);
+
+    List<Product> getProducts();
+
+    Product getProductById(String id);
+}
