c60c5de68e79f9e51d50bf124c16ceb73e5db0c5
Ansu <aansumandas345@gmail.com>
2024-09-18 07:19:53 +0530

    product request add

---
+++ backend/src/main/java/com/ApparelAvenue/backend/dto/ProductRequestDto.java
@@ -0,0 +1,13 @@
+package com.ApparelAvenue.backend.dto;
+
+import com.ApparelAvenue.backend.constant.Section;
+
+public class ProductRequestDto {
+    private String productName;
+    private String productDescription;
+    private String productImage;
+    private String productQuantity;
+    private String productMrp;
+    private String productSellingPrice;
+    private Section section;
+}
