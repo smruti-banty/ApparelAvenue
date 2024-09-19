277ca303bd8c095bedf8a6f97fd35cbbe8bde682
nitish <nitish@2003>
2024-09-19 13:11:41 +0530

    deleted all product

--- backend/src/main/java/com/ApparelAvenue/backend/controller/ProductController.java
+++ backend/src/main/java/com/ApparelAvenue/backend/controller/ProductController.java
@@ -1,9 +1,19 @@
 package com.ApparelAvenue.backend.controller;
 
+import com.ApparelAvenue.backend.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+    @DeleteMapping("/all")
+    public void deleteAllProduct() {
+        productService.deleteAllProduct();
+    }
 }
--- backend/src/main/java/com/ApparelAvenue/backend/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/com/ApparelAvenue/backend/service/impl/ProductServiceImpl.java
@@ -1,4 +1,61 @@
 package com.ApparelAvenue.backend.service.impl;
 
-public class ProductServiceImpl {
+import com.ApparelAvenue.backend.model.Product;
+import com.ApparelAvenue.backend.repository.ProductRepository;
+import com.ApparelAvenue.backend.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+
+    @Override
+    public Product createProduct() {
+        return null;
+    }
+
+    @Override
+    public Product updateProduct(String id, Product newProduct) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllProduct() {
+            productRepository.deleteAll();
+    }
+
+    @Override
+    public Product deleteProductById(String id) {
+        return null;
+    }
+
+    @Override
+    public Product increaseProductQuantity(String id, int quantity) {
+        return null;
+    }
+
+    @Override
+    public Product decreaseProductQuantity(String id, int quantity) {
+        return null;
+    }
+
+    @Override
+    public Product updateProductPrice(String id, double price) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(String id) {
+        return null;
+    }
 }
