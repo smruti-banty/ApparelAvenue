9829a51486bc4c99d251f4472561b5a291c0d168
Bicky <gwbicky@gmail.com>
2024-09-19 10:12:29 +0530

    product delete by id

--- backend/src/main/java/com/ApparelAvenue/backend/controller/ProductController.java
+++ backend/src/main/java/com/ApparelAvenue/backend/controller/ProductController.java
@@ -1,9 +1,27 @@
 package com.ApparelAvenue.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ApparelAvenue.backend.model.Product;
+import com.ApparelAvenue.backend.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService service;
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Product> deleteProductById(@PathVariable String id) {
+
+        Product product = service.deleteProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.NO_CONTENT);
+    }
 }
--- backend/src/main/java/com/ApparelAvenue/backend/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/com/ApparelAvenue/backend/service/impl/ProductServiceImpl.java
@@ -1,4 +1,76 @@
 package com.ApparelAvenue.backend.service.impl;
 
-public class ProductServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ApparelAvenue.backend.model.Product;
+import com.ApparelAvenue.backend.repository.ProductRepository;
+import com.ApparelAvenue.backend.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository repository;
+
+    @Override
+    public Product createProduct() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createProduct'");
+    }
+
+    @Override
+    public Product updateProduct(String id, Product newProduct) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
+    }
+
+    @Override
+    public void deleteAllProduct() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllProduct'");
+    }
+
+    @Override
+    public Product deleteProductById(String id) {
+        if (!repository.existsById(id)) {
+            throw new IllegalArgumentException(id + "does not exist.");
+        }
+        var product = repository.findById(id).get();
+        repository.delete(product);
+        return product;
+    }
+
+    @Override
+    public Product increaseProductQuantity(String id, int quantity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'increaseProductQuantity'");
+    }
+
+    @Override
+    public Product decreaseProductQuantity(String id, int quantity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'decreaseProductQuantity'");
+    }
+
+    @Override
+    public Product updateProductPrice(String id, double price) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProductPrice'");
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProducts'");
+    }
+
+    @Override
+    public Product getProductById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductById'");
+    }
+
 }
