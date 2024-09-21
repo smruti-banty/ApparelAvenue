107d9d54a2cf9d1000d8662ee969196bd2551e71
smita <smitamayeebehera291@gmail.com>
2024-09-21 07:52:26 +0530

    get all products

--- backend/src/main/java/com/ApparelAvenue/backend/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/com/ApparelAvenue/backend/service/impl/ProductServiceImpl.java
@@ -5,10 +5,11 @@ import com.ApparelAvenue.backend.repository.ProductRepository;
 import com.ApparelAvenue.backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository repository;
 
