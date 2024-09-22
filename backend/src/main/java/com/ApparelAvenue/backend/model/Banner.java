2fd70e54793d67bb45d84085b6d056d0fb773a3f
tapas2468 <tapasrout414@gmail.com>
2024-09-22 12:34:19 +0530

    Banner Model class add

---
+++ backend/src/main/java/com/ApparelAvenue/backend/model/Banner.java
@@ -0,0 +1,22 @@
+package com.ApparelAvenue.backend.model;
+
+import com.ApparelAvenue.backend.constant.Section;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Banner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String bannerId;
+    private String bannerImage;
+    @Enumerated(value = EnumType.STRING)
+    private Section section;
+}
