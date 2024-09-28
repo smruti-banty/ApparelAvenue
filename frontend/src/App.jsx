24508878d245f8cd8f8d5a62d8b372c083982d03
nitish <nitish@2003>
2024-09-28 07:04:28 +0530

    design Signup page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Signup from './pages/Signup.jsx';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup /> 
+    </div>
+  );
+}
+
+export default App;
